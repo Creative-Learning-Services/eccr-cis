4ecb4f630cfb4408e21fae09ff7ab5419105b094
JDTobin <james.d.tobin3.ctr@mail.mil>
2026-01-08 15:08:07 -0500

    remove unused files and old app
    Signed-off-by: JDTobin <james.d.tobin3.ctr@mail.mil>

--- app/CIS/urls.py
+++ app/CIS/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
@@ -25,7 +23,6 @@ from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
 urlpatterns = [
     path("admin/", admin.site.urls),
     # API endpoints
-    path("api/", include("eccr.urls")),
     path("api/", include("competencies.urls")),
     # OpenAPI schema and documentation
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
--- app/eccr/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EccrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eccr'
